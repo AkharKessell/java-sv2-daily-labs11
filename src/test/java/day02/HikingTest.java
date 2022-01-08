f9bbb7946108d1f0dfd424430381a334de9b515f
Attila Gönczi-Kovács <rogkacs@gmail.com>
2022-01-08 14:01:58 +0100

    A day02 csomagban dolgozz!
    Túrázás közben egy GPS eszköz rögzíti a pontokat.  Eltárolja a GPS koordinátákat, valamint a magasságot (mindegyik  lebegőpontos érték). Írj a Hiking osztályba egy getPlusElevation() metódust, mely megkapja a magasságok listáját, és visszaadja a  emelkedések összegét. Azaz pl. 10,20,15,18 esetén 13, ugyanis (20 - 10) + (18 - 15). A 20 méterről 15 méterre ereszkedést nem számolja bele,  hiszen az ereszkedés, és nem emelkedés.

---
+++ src/test/java/day02/HikingTest.java
@@ -0,0 +1,30 @@
+package day02;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class HikingTest {
+    public static final List<GpsCoordinate> COORDINATES =
+            List.of(new GpsCoordinate(33, 10, 10), new GpsCoordinate(45, 66, 20),
+                    new GpsCoordinate(91, 58, 15), new GpsCoordinate(88, 40, 18));
+
+    @Test
+    void testGetElevation() {
+        Hiking hiking = new Hiking(COORDINATES);
+
+        assertEquals(13, hiking.getPlusElevation(hiking.getCoordinates()));
+    }
+
+    @Test
+    void testGetElevationEmptyList() {
+        Hiking hiking = new Hiking(null);
+
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class,
+                () -> hiking.getPlusElevation(null));
+        assertEquals("Input list is empty.", iae.getMessage());
+    }
+}
