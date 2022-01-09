f0b032eaa60a9c1c9e3b84332f4fcf5dc56c81c3
Attila Gönczi-Kovács <rogkacs@gmail.com>
2022-01-09 14:20:56 +0100

    Készíts a day05 csomagban egy Lottery  osztályt, melynek két attribútuma az, hogy hány számból kell húzni és  mennyit húznak. Legyen egy startLottery() metódusa, ami visszaad egy  listát amiben annyi véletlen szám van amennyit húzni kell a határnak  megfelelően. A sorrendre nem kell figyelni, a lényeg, hogy a számok  különbözők legyenek!

---
+++ src/test/java/day05/LotteryTest.java
@@ -0,0 +1,22 @@
+package day05;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LotteryTest {
+
+    @Test
+    void testStartLottery() {
+        Lottery lottery = new Lottery(90, 5, new Random(1));
+        assertEquals(5, lottery.startLottery().size());
+    }
+
+    @Test
+    void testStartLottery2() {
+        Lottery lottery = new Lottery(5, 5, new Random(1));
+        assertEquals(5, lottery.startLottery().size());
+    }
+}
